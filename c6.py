9838c195108c2fd3905c4f221b9a0de496f55a69
ChavivaK <145359782+ChavivaK@users.noreply.github.com>
2024-08-01 14:45:43 +0300

    Update c6.py

--- c6.py
+++ c6.py
@@ -25,20 +25,20 @@ class C6Lattice(Lattice):
             assert y < self._y_length - 1
             assert z < self._z_length - 1
             return [[
-                        ((x, y, z), C6CellAlignments.ZX),
-                        ((x, y, z), C6CellAlignments.YZ)
-                    ],
-                    [
                         ((x, y + 1, z), C6CellAlignments.ZX),
-                        ((x, y + 1, z), C6CellAlignments.YX)
+                        ((x, y + 1, z), C6CellAlignments.YZ)
                     ],
                     [
-                        ((x, y, z + 1), C6CellAlignments.ZY),
-                        ((x, y, z + 1), C6CellAlignments.YZ)
+                        ((x, y, z), C6CellAlignments.ZX),
+                        ((x, y, z), C6CellAlignments.YX)
                     ],
                     [
                         ((x, y + 1, z + 1), C6CellAlignments.ZY),
-                        ((x, y + 1, z + 1), C6CellAlignments.YX)
+                        ((x, y + 1, z + 1), C6CellAlignments.YZ)
+                    ],
+                    [
+                        ((x, y, z + 1), C6CellAlignments.ZY),
+                        ((x, y, z + 1), C6CellAlignments.YX)
                     ]]
         if a == 1:
             assert x < self._x_length - 1
